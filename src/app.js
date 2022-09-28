fd374ae63ebaf07a18f210ba5ac8be825583f176
Bianca11810 <Bianca11810@gmail.com>
2022-09-28 20:18:31 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".numValue").innerHTML = genRanNum();
+  document.querySelector(".card").classList.add(genRanSuit());
+};
+
+let genRanNum = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let ranNum = Math.floor(Math.random() * number.length);
+  return number[ranNum];
+};
+
+let genRanSuit = () => {
+  let suit = ["diams", "spade", "hearts", "clubs"];
+  let ranSuit = Math.floor(Math.random() * suit.length);
+  return suit[ranSuit];
 };
